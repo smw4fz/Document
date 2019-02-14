796eeae84c92a2c499e499425c8b3dae3ce21eec
smw4fz <46974661+smw4fz@users.noreply.github.com>
2019-02-14 10:25:55 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO; 
+
+namespace Document_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+                Console.WriteLine("Document");
+                Console.WriteLine("\nWhat is the name of the document?");
+                string name = Console.ReadLine(); 
+                Console.WriteLine("What is the content in {0}?", name);
+                string content = Console.ReadLine();
+    
+        try
+            {
+                StreamWriter sw = new StreamWriter("C:\\name.txt");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            } 
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            } 
+        } 
+    }
+}
